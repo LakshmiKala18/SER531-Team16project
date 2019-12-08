22a8ff5e8351f060e1f12c25afcf1356186c49f4
JKDrumm <jkdrumm1@gmail.com>
2019-12-07 19:10:03 -0700

    Allowed users to click on event id to get tweets near event

--- app/routes.py
+++ app/routes.py
@@ -15,14 +15,17 @@ def index():
         PREFIX userdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/11/userdata#>
         PREFIX tweetdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/11/TweetonEvent#>
         PREFIX eventcat: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/10/eventcategory#>
-        SELECT DISTINCT ?userid
+        SELECT DISTINCT ?userid ?user_name
         WHERE {
-            ?x userdata:has_ID ?userid
+            ?user userdata:has_ID ?userid.
+            ?user userdata:has_name ?user_name.
         }""")
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     for uid in results['results']['bindings']:
-        u = [uid['userid']['value']]
+        u = []
+        u.append(uid['userid']['value'])
+        u.append(uid['user_name']['value'])
         uids.append(u)
     return render_template('welcome.html', uids=uids)
 
@@ -46,25 +49,22 @@ PREFIX tweetdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/
 PREFIX eventcat: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/10/eventcategory#>
 SELECT distinct ?eventid ?eventime ?eventcity ?eventcat 
 WHERE {
-  ?y eventcat:has_event_id ?eventid;
-     eventcat:is_of_category ?eventcat;
-     eventcat:at_city ?eventcity;
-     eventcat:has_start_time ?eventime;
-      eventcat:has_longitude ?elong;
-      eventcat:has_latitude ?elat.
- 
-  SERVICE <http://34.70.187.222:3030/projecttweetdata/query>{
-  SELECT ?lat ?long 
-WHERE {
-  ?z tweetdata:has_latitude ?lat;
-      tweetdata:tweeted_userID ?tuserid;
-      tweetdata:has_longitude ?long.
-      FILTER(xsd:float(?tuserid) = "%f"^^xsd:float).
+    SERVICE <http://34.70.187.222:3030/projecttweetdata/query>{
+        SELECT ?lat ?long 
+        WHERE {
+        ?z tweetdata:has_latitude ?lat;
+            tweetdata:tweeted_userID ?tuserid;
+            tweetdata:has_longitude ?long.
+            FILTER(xsd:float(?tuserid) = "%f"^^xsd:float).
+        }
     }
-    }
-    FILTER((abs(xsd:float(?lat)) - abs(xsd:float(?elong)) < 1) && (abs(xsd:float(?long)) - abs(xsd:float(?elat)) < 1)).
-   
-
+    ?y eventcat:has_event_id ?eventid;
+        eventcat:is_of_category ?eventcat;
+        eventcat:at_city ?eventcity;
+        eventcat:has_start_time ?eventime;
+        eventcat:has_longitude ?elong;
+        eventcat:has_latitude ?elat.
+    FILTER((abs(xsd:float(?lat) - xsd:float(?elong)) < 1) && (abs(xsd:float(?long) - xsd:float(?elat)) < 1)).
  }limit 10""" % (userid))
     sparql.setReturnFormat(JSON)
     # sparql.method = "GET"
@@ -75,7 +75,6 @@ WHERE {
              tweet['eventcat']['value']]
         tweets.append(t)
     return render_template('search2.html', tweets=tweets)
-
 
 @app.route('/search')
 def search():
@@ -108,5 +107,68 @@ def search():
              tweet['eventcat']['value']]
         tweets.append(t)
     return render_template('search.html', tweets=tweets)
+
+@app.route('/event', methods=['POST'])
+def event():
+    eventID = request.form.get('eventID')
+    tweets = []
+    sparql = SPARQLWrapper("http://104.197.168.147:3030/eventcategory/query")
+    sparql.setQuery(
+        """PREFIX userdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/11/userdata#>
+            PREFIX tweetdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/11/TweetonEvent#>
+            PREFIX eventcat: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/10/eventcategory#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+            SELECT ?elat ?elong
+            WHERE {
+                ?event eventcat:has_event_id ?eventid;
+                    eventcat:has_longitude ?elong;
+                    eventcat:has_latitude ?elat.
+                    FILTER(xsd:float(?eventid) = "%s"^^xsd:float).
+            }
+            LIMIT 1""" % (eventID))
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()['results']['bindings'][0]
+    lat = float(results['elat']['value'])
+    lng = float(results['elong']['value'])
+    print(lat)
+    print(lng)
+    sparql = SPARQLWrapper("http://34.70.187.222:3030/projecttweetdata/query")
+    sparql.setQuery(
+        """PREFIX userdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/11/userdata#>
+            PREFIX tweetdata: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/11/TweetonEvent#>
+            PREFIX eventcat: <http://www.semanticweb.org/raghavakannikanti/ontologies/2019/10/eventcategory#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+            SELECT ?tweet_text ?name ?tweet_date
+            WHERE {
+                #SERVICE <http://35.184.170.79:3030/userdata/query>
+                #{
+                #    ?user userdata:has_ID ?user_id.
+                #    ?user userdata:has_name ?name.
+                #}
+                ?tweet rdf:type tweetdata:Tweet.
+                ?tweet tweetdata:tweet_text ?tweet_text.
+                ?tweet tweetdata:tweet_date ?tweet_date.
+                ?tweet tweetdata:tweeted_userID ?user_id2.
+                ?tweet tweetdata:has_latitude ?tweet_lat.
+                ?tweet tweetdata:has_longitude ?tweet_lng.
+                FILTER((abs(xsd:float(?tweet_lat) - "%f"^^xsd:float) < 0.5) && (abs(xsd:float(?tweet_lng) - "%f"^^xsd:float) < 0.5)).
+                #FILTER(xsd:float(?user_id) = xsd:float(?user_id2)).
+            }
+            LIMIT 10""" % (lng, lat))
+    sparql.setReturnFormat(JSON)
+    # sparql.method = "GET"
+    results = sparql.query().convert()
+    for tweet in results['results']['bindings']:
+        t = [tweet['tweet_text']['value'], tweet['tweet_date']['value']]
+        tweets.append(t)
+    return render_template('event.html', tweets=tweets)
 
 # FILTER(?a (xsd:float(?lat) xsd:float(?long)) spatial:nearby(xsd:float(?elong) xsd:float(?elat) 10 'mi')).
